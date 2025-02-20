24930760b57d8c2ee51e0ed81aa3cfb662ef4db7
Shellbaka <shellbaka69@gmail.com>
2025-02-20 16:03:26 -0300

    Projeto com JWT e Spring security e já deixei feito um user aqui, agora vou estabelecer o repository e as outras packages.

---
+++ src/test/java/DIO/Spring_Security_JWT/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package DIO.Spring_Security_JWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
